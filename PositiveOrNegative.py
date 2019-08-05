8c97bba7d144f6fb316610a156f8d086cb96d705
antoabijit <53575856+antoabijit@users.noreply.github.com>
2019-08-05 06:19:33 -0700

    Create PositiveOrNegative.py

---
+++ PositiveOrNegative.py
@@ -0,0 +1,9 @@
+num=int(input("Enter any number : "))
+if num>0:
+    print(num," is Positive number.")
+elif num==0:
+    print(num,".It is Zero.")
+elif num<0:
+    print(num," is Negative number.")
+else:
+    print("Invalid Entry")
