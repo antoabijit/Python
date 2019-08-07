25640e0d36363ff7ee66a621f754b69d6d034d02
antoabijit <53575856+antoabijit@users.noreply.github.com>
2019-08-07 10:18:33 -0700

    Add files via upload

---
+++ 4th fl.py
@@ -0,0 +1,5 @@
+#Python program to get the 4th element and 4th element from last of a tuple
+a=input().split(",")
+b=tuple(a)
+print(b[3])
+print(b[-4])
---
+++ add item tup.py
@@ -0,0 +1,6 @@
+#Python program to add an item in a tuple
+a=input().split(",")
+b=tuple(a)
+print(b)
+b=b+(9,0)
+print(b)
