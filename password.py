d56ad417d4a65d1a0ccd1e3f08ed86b2a1970cec
antoabijit <53575856+antoabijit@users.noreply.github.com>
2019-08-07 10:28:53 -0700

    Create password.py

---
+++ password.py
@@ -0,0 +1,22 @@
+import re
+password= str(input("Input your password: "))
+x=True
+while x:
+    if (len(password)<6 or len(password)>12):
+        break
+    elif not re.search("[a-z]",password):
+        break
+    elif not re.search("[0-9]",password):
+        break
+    elif not re.search("[A-Z]",password):
+        break
+    elif not re.search("[$#@]",password):
+        break
+    elif re.search("\s",password):
+        break
+    else:
+        print("Valid Password")
+        x=False
+        break
+    if x:
+      print("not a valid password")
